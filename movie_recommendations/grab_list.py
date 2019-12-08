9aeb9505984c2004c5c249d261722288e702cf80
Yandi Jin <yandijin@uw.edu>
2019-12-08 12:26:31 -0800

    Update movie_recommendations/grab_list.py

--- movie_recommendations/grab_list.py
+++ movie_recommendations/grab_list.py
@@ -1,12 +1,19 @@
+'''
+this is a function
+'''
+
 import csv
 import sys
 import pickle
 import os.path
 
 
+max_count = 50000
+pop_director = 10
+pop_actor = 30
+
 
 genre_set = {'Foreign'}
-#year_set = {}
 country_set = {'United States of America'}
 director_set = {'Mervyn LeRoy'}
 actor_set = {'Sho Kosugi'}
@@ -15,11 +22,6 @@ actor_dic = {'Sho Kosugi': 0}
 name_set = {'Stagecoach'}
 id_set = {2}
 id_title_set = {'Annie Hall': 703}
-max_count = 50000
-pop_director = 10
-pop_actor = 30
-
-
 
 
 Run = False
@@ -32,6 +34,9 @@ if __name__ == "__main__":
 
 
 def process_genre(row):
+    '''
+    this is a function
+    '''
     substring = row['genres']
     index = 100000
     if substring is None:
@@ -39,28 +44,38 @@ def process_genre(row):
     while index > 0:
         index = substring.find('name')
         if index > 0:
-           substring = substring[index + 8:]
-           index_end = substring.find('\'')
-           genre = substring[0:index_end]
-           genre_set.add(genre)
+            substring = substring[index + 8:]
+            index_end = substring.find('\'')
+            genre = substring[0:index_end]
+            genre_set.add(genre)
+
 
 def process_name(row):
+    '''
+    this is a function
+    '''
     substring = row['title']
-    index = 100000
     if substring is None:
         return
     if len(substring) > 0 and substring[0].isalpha():
         name_set.add(substring)
 
+
 def process_id(row):
+    '''
+    this is a function
+    '''
     substring = row['id']
     if len(substring) > 0 and substring.isnumeric():
         id_set.add(int(substring))
 
+
 def process_title_id(row):
+    '''
+    this is a function
+    '''
     substring = row['id']
     substringg = row['title']
-    index = 100000
     if substring is None or substringg is None:
         return
     if len(substringg) > 0 and substringg[0].isalpha() and len(substring) > 0 and substring.isnumeric():
@@ -68,6 +83,9 @@ def process_title_id(row):
 
 
 def process_country(row):
+    '''
+    this is a function
+    '''
     substring = row['production_countries']
     if substring is None:
         return
@@ -75,12 +93,16 @@ def process_country(row):
     while index > 0:
         index = substring.find('name')
         if index > 0:
-           substring = substring[index + 8:]
-           index_end = substring.find('\'')
-           country = substring[0:index_end]
-           country_set.add(country)
+            substring = substring[index + 8:]
+            index_end = substring.find('\'')
+            country = substring[0:index_end]
+            country_set.add(country)
+
 
 def process_director(row):
+    '''
+    this is a function
+    '''
     substring = row['crew']
     if substring is None:
         return
@@ -94,7 +116,7 @@ def process_director(row):
         director = director[1:]
     if director is not None and director[0].isalpha():
         if director in director_dic:
-            director_dic[director]  = director_dic[director] + 1;
+            director_dic[director] = director_dic[director] + 1
             if director_dic[director] > pop_director:
                 director_set.add(director)
         else:
@@ -102,6 +124,9 @@ def process_director(row):
 
 
 def process_cast(row):
+    '''
+    this is a function
+    '''
     substring = row['cast']
     if substring is None:
         return
@@ -109,40 +134,38 @@ def process_cast(row):
     while index > 0:
         index = substring.find('name')
         if index > 0:
-           substring = substring[index + 8:]
-           index_end = substring.find('\'')
-           actor = substring[0:index_end]
-           if actor is None or len(actor) < 1:
-               pass;
-           else:
-               if actor[0] == ' ':
-                   actor = actor[1:]
-               if  len(actor) > 0 and actor[0].isalpha():
-                   if actor in actor_dic:
-                       actor_dic[actor] = actor_dic[actor] + 1
-                       if actor_dic[actor] > pop_actor:
-                           actor_set.add(actor)
-                   else:
-                       actor_dic[actor] = 1
+            substring = substring[index + 8:]
+            index_end = substring.find('\'')
+            actor = substring[0:index_end]
+            if actor is None or len(actor) < 1:
+                pass
+            else:
+                if actor[0] == ' ':
+                    actor = actor[1:]
+                if  len(actor) > 0 and actor[0].isalpha():
+                    if actor in actor_dic:
+                        actor_dic[actor] = actor_dic[actor] + 1
+                        if actor_dic[actor] > pop_actor:
+                            actor_set.add(actor)
+                    else:
+                        actor_dic[actor] = 1
 
-    '''
-    index = substring.find('name\'')
-    substring = substring[index + 8:]
-    index = substring.find('\'')
-    director = substring[0:index]
-    if director is not None:
-        director_set.add(director)
-'''
 
 def read_csv():
-    file = open('meta.txt','rb')
+    '''
+    this is a function
+    '''
+    file = open('meta.txt', 'rb')
     obs = pickle.load(file)
     return obs
 
+
 def read_csv_c():
+    '''
+    this is a function
+    '''
     with open('movies_metadata.csv', mode='r', encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        line_count = 0
         for row in csv_reader:
             process_genre(row)
             process_country(row)
@@ -152,47 +175,44 @@ def read_csv_c():
 
     with open('credits.csv', mode='r', encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        line_count = 0
         for row in csv_reader:
             process_director(row)
             process_cast(row)
-"""
-country_set = {'United States of America'}
-director_set = {'Mervyn LeRoy'}
-actor_set = {'Sho Kosugi'}
-director_dic = {'Mervyn LeRoy': 0}
-actor_dic = {'Sho Kosugi': 0}
-name_set = {'Stagecoach'}
-id_set = {2}
-id_title_set = {'Annie Hall': 703}
-"""
+
+
 class object_save:
-    def __init__(self, genre_set, country_set, director_set, actor_set, director_dic, actor_dic, name_set, id_set, id_title_set):
-        self.country_set = sorted(country_set)
-        self.genre_set = sorted(genre_set)
-        self.director_set = sorted(director_set)
-        self.actor_set = sorted(actor_set)
-        self.director_dic = (director_dic)
-        self.actor_dic = (actor_dic)
-        self.name_set = sorted(name_set)
-        self.id_set = sorted(id_set)
-        self.id_title_set = (id_title_set)
+    '''
+    this is a class
+    '''
+    def __init__(self, genre_set_o, country_set_o, director_set_o, actor_set_o, director_dic_o, actor_dic_o, name_set_o, id_set_o, id_title_set_o):
+        self.country_set = sorted(country_set_o)
+        self.genre_set = sorted(genre_set_o)
+        self.director_set = sorted(director_set_o)
+        self.actor_set = sorted(actor_set_o)
+        self.director_dic = (director_dic_o)
+        self.actor_dic = (actor_dic_o)
+        self.name_set = sorted(name_set_o)
+        self.id_set = sorted(id_set_o)
+        self.id_title_set = (id_title_set_o)
 
 
 def save_file():
-
+    '''
+    this is a function
+    '''
     obs = object_save(genre_set, country_set, director_set, actor_set, director_dic, actor_dic, name_set, id_set, id_title_set)
-    file = open('meta.txt','wb')
+    file = open('meta.txt', 'wb')
     pickle.dump(obs, file)
     print('file saved: meta.txt')
 
 
-
-
 if Run:
+    '''
+    this is a function
+    '''
     read_csv_c()
-    #print(id_title_set)
     save_file()
+
 #read_csv()
 #print(id_title_set)
 #print(name_set)
