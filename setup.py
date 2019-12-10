d601b3dbe1fef28476b833f8a20b631567ada644
Mewoy666 <46179760+Mewoy666@users.noreply.github.com>
2019-12-09 18:17:30 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,19 +11,19 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 opts = dict(name='CSE583project-Group9',
-            version='1.0.0',
+            #version='1.0.0',
             description='Give movie recommendations based on needs',
             long_description=long_description,
             url='https://github.com/xiashuhan/CSE583project-Group9',
             license=open('LICENSE').read(),
             author='Sizhe Chen, Yandi Jin, Miaoran Li, Shuhan Xia, Jing Xu',
             packages=find_packages(),
-            install_requires=['ast', 'collections', 'dash',
-                              'dash_bootstrap_components',
-                              'dash_core_components',
-                              'dash_html_components', 'http',
-                              'json', 'nltk', 'numpy', 'pandas',
-                              'pickle', 'sklearn', 'surprise'],
+            #install_requires=['ast', 'collections', 'dash',
+            #                  'dash_bootstrap_components',
+            #                  'dash_core_components',
+            #                  'dash_html_components', 'http',
+            #                  'json', 'nltk', 'numpy', 'pandas',
+            #                  'pickle', 'sklearn', 'surprise'],
             package_data={'movie_recommendations': ['movies-dataset/*.*',
                                                     'movies-dataset/source/*.*']}
             )
